7b9c09faa8cac9d4cf0f8c6f14942b1a410078f7
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2020-11-17 19:11:30 -0300

    exercícios de interpretação de código

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,7 @@
+/* Exercícios de interpretação de código
+
+1. O primeiro console.log imprime 10, enquanto o segundo imprime 10 e 5, já que a variável b foi mudada durante o proceddo
+
+2. Vai imprimir o valor 10 três vezes, pois a variável b muda de valor e anula o 20 inicial, sendo igual à c, que é igual à a, que é 10. */
+
+// 

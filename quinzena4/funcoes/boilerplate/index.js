bd187953a2a62eacae453cb432723ed406f96000
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2020-12-01 21:31:45 -0300

    Exercício de interpretação de código concluídos

---
+++ quinzena4/funcoes/boilerplate/index.js
@@ -0,0 +1,15 @@
+// console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+// Exercícios de interpretação de código
+
+// EXERCÍCIO 1
+// a. Serão impressos os números 10 e 50, já que é 2 * 5 e 10 * 5.
+// b. Não apareceria nada, pois o console.log serve justamente para imprimir os valores no console.
+
+// EXERCÍCIO 2
+// a. Os nomes Darvas e Goli, já que o loop roda enquanto o resultado for menor do que 2. Impressos 2 itens da array, ele para de rodar.
+// b. Os nomes Amanda e Caio, pelos mesmos motivos citados no item logo acima.
+
+// EXERCÍCIO 3
+// RESPOSTA: Se um número a ser especificado na função tiver o resto de divisão igual a 0, será incluído na array, inicialmente vazia, o número multiplicado por ele mesmo. Uma possível alternativa para renomear a arrayFinal seria numeroAoQuadrado.
+

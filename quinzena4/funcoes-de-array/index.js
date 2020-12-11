29c894d57367cd6edd3f66cec3cc8cc54e70aeda
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2020-12-11 01:00:52 -0300

    Exercícios concluídos sem desafios (por hora)

---
+++ quinzena4/funcoes-de-array/index.js
@@ -0,0 +1,158 @@
+let arrDespesas = []
+imprimirDespesas(arrDespesas)
+imprimirExtrato()
+
+
+// PRIMEIRO
+function imprimirDespesas(despesas){
+    let divDespesas = document.getElementById('despesas')
+    divDespesas.innerHTML = '<p><u>Despesas Detalhadas</u></p>'
+
+    // AQUI VEM A IMPLEMENTAÇÃO
+    const camposDespesas = (despesas) => {
+        divDespesas.innerHTML += `<p>Valor: R$ ${despesas.valor} - Tipo: ${despesas.tipo} - Descrição: ${despesas.descricao}</p>`
+    }
+    despesas.map(camposDespesas)
+}
+
+
+// SEGUNDO 
+function imprimirExtrato(){
+    let divExtrato = document.getElementById('extrato')
+    let gastoTotal = 0
+    let gastoAlimentacao = 0
+    let gastoUtilidades = 0
+    let gastoViagem = 0
+
+
+    // AQUI VEM A IMPLEMENTAÇÃO
+    arrDespesas.forEach((tipoExtrato) => {
+        switch(tipoExtrato.tipo) {
+            case "alimentação":
+                gastoAlimentacao += tipoExtrato.valor
+                break;
+            case "utilidades":
+                gastoUtilidades += tipoExtrato.valor
+                break;
+            case "viagem":
+                gastoViagem += tipoExtrato.valor
+                break;
+        }
+
+        gastoTotal += tipoExtrato.valor
+    })
+
+    divExtrato.innerHTML = `<p>Extrato: Gasto Total: R$${gastoTotal} | Alimentação: R$${gastoAlimentacao} | 
+                                        Utilidades: R$${gastoUtilidades} | Viagem: R$${gastoViagem}</p>`
+}
+
+
+function limparFiltros() {
+    document.getElementById('tipoFiltro').value = ""
+    document.getElementById('valorFiltroMin').value = ""
+    document.getElementById('valorFiltroMax').value = ""
+}
+
+
+
+function adicionarDespesa(){
+    let valorCdt = document.getElementById('valorCadastro')
+    let tipoCtd = document.getElementById('tipoCadastro')
+    let descricaoCtd = document.getElementById('descricaoCadastro')
+
+    if(validarValor(valorCdt) && validarTipo(tipoCtd) && validarDescricao(descricaoCtd)){
+        let novaDespesa = {
+            valor: Number(valorCdt.value),
+            tipo: tipoCtd.value,
+            descricao: descricaoCtd.value,
+        }
+
+        arrDespesas.push(novaDespesa)
+        
+        valorCdt.value = ""
+        tipoCtd.value = ""
+        descricaoCtd.value = ""
+
+        
+        limparFiltros()
+        imprimirDespesas(arrDespesas)
+        imprimirExtrato()
+    } else {
+        alert("Faltou algum valor ou algum valor é um número negativo")
+    }
+}
+
+
+
+// TERCEIRO
+function filtrarDespesas(){
+    let tipoFiltro = document.getElementById('tipoFiltro').value
+    let valorMin = Number(document.getElementById('valorFiltroMin').value)
+    let valorMax = Number(document.getElementById('valorFiltroMax').value)
+
+    if(tipoFiltro == "" && valorMin == "" && valorMax == "") {
+        alert("Defina um filtro!")
+    }
+
+    let condicionaisFiltros = (filtros) => {
+
+        if(tipoFiltro === "todos") {
+            if(valorMin > 0 && valorMax >= 0) {
+                if(valorMin <= filtros.valor && valorMax >= filtros.valor) {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                return true
+            }
+        } else if(filtros.tipo === tipoFiltro) {
+            if(valorMin > 0 && valorMax >= 0) {
+                if(valorMin <= filtros.valor && valorMax >= filtros.valor) {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+
+    let despesasFiltradas = arrDespesas.filter(condicionaisFiltros) // AQUI NESSA VARIÁVEL VEM A IMPLEMENTAÇÃO
+
+    imprimirDespesas(despesasFiltradas)
+}
+
+
+
+
+
+
+
+// FunÇoes que fazem validaÇoes dos inputs de criaÇao de despesas 
+
+// NÃO SE PREOCUPEM EM ENTENDER ESSAS FUNÇÕES
+
+function validarValor(valor){
+    if(valor.value.length > 0 && parseInt(valor.value) > 0){
+        return true
+    }
+    return false
+}
+
+function validarTipo(tipo){
+    if(tipo.value !== ""){
+        return true
+    }
+    return false
+}
+
+function validarDescricao(texto){
+    if(texto.value.replace(/ /g,"").length !== 0){
+        return true
+    }
+    return false
+}

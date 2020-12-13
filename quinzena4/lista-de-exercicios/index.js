236d81b973ca0c9a5b5e52a3eaf09be1cf775aeb
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2020-12-13 16:28:35 -0300

    Exercício 1 de interpretação de código concluído

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,36 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+// EXERCÍCIO 1
+
+/* O que faz: Solicita que o usuário digite o valor da cotação do dólar no dia e converte 100 dólares de acordo com o valor da cotação em reais.
+
+Como faz: A função conversorDeMoeda tem parâmetro valorEmDolar e, dentro dela, há uma variável chamada "cotacao" com prompt para informar valor da cotação em dólar, que é transformado em número. O retorno é o parâmetro da função multiplicado pela cotação digitada pelo usuário para chegar ao valor em R$. Depois, fora da função, a variável "meuDinheiro" tem a função com o parâmetro definido em 100 e um console.log que imprime o resultado.
+
+Qual será o valor impresso no console: Hoje, 13/12/2020, a cotação do dólar é R$5,07. O valor impresso, portanto, é de R$ 507. */
+
+
+// EXERCÍCIO 2
+
+/* O que faz: 
+
+Como faz: 
+
+Qual será o valor impresso no console:  */
+
+
+// EXERCÍCIO 3
+
+/* O que faz: 
+
+Como faz: 
+
+Qual será o valor impresso no console:  */
+
+
+// EXERCÍCIO 4
+
+/* O que faz: 
+
+Como faz: 
+
+Qual será o valor impresso no console:  */

12d0bdcb5ddc5aa9f062e8ea4258cc1f29bf023a
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2020-12-13 21:19:05 -0300

    Exercício 5 de funções de array concluído

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -318,3 +318,24 @@ function enviarMensagem(consultas) {
 }
 
 console.log(enviarMensagem(consultas)) */
+
+
+// EXERCÍCIO 5
+
+/* const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+contas.map((saldoAtualizado) => {
+
+    for (let i = 0; i < saldoAtualizado.compras.length; i++) {
+        saldoAtualizado.saldoTotal -= saldoAtualizado.compras[i]
+    } 
+ })
+
+ console.log(contas) */

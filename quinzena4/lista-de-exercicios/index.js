bf72615c4c53ac0f4631a8c308eb4f90b57fdcbd
Ricardo Ramos <ricardoalmeidaramos@gmail.com>
2020-12-13 21:05:19 -0300

    Exercício 3 de funções de array concluído

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -263,3 +263,27 @@ console.log(multiplicaPor3) */
 })
 
 console.log(String(verificaParOuImpar)) */
+
+
+// EXERCÍCIO 3
+
+const pessoas = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+/* a) const entradaPermitida = pessoas.filter((podeOuNao) => {
+    return podeOuNao.idade >= 14 && podeOuNao.idade <= 60 && podeOuNao.altura >= 1.5
+})
+
+console.log(entradaPermitida) */
+
+/* b) const entradaProibida = pessoas.filter((podeOuNao) => {
+    return podeOuNao.idade < 14 || podeOuNao.idade > 60 || podeOuNao.altura < 1.5
+})
+
+console.log(entradaProibida) */
